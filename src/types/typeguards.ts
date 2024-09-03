36124fa774a4aefe1b1bf63c90f73c262bb2b0b5
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-03 20:35:37 +0100

    chore: typeguards and error helpers

--- src/types/typeguards.ts
+++ src/types/typeguards.ts
@@ -1,6 +1,7 @@
 import { Update } from "@grammyjs/types";
 import { Context } from "./context";
 import { PluginInputs } from "./plugin-inputs";
+import { WORKFLOW_FUNCTIONS } from "../workflow-functions";
 
 export function isIssueOpenedEvent(context: Context): context is Context<"issues.opened"> {
   return context.eventName === "issues.opened";
@@ -20,4 +21,13 @@ export function isGithubPayload(inputs: any): inputs is PluginInputs {
   } catch (e) {
     return false;
   }
+}
+
+
+export function isIssueLabeledEvent(context: Context): context is Context<"issues.labeled"> {
+  return context.eventName === "issues.labeled";
+}
+
+export function isWorkflowFunction(event: string): event is keyof typeof WORKFLOW_FUNCTIONS {
+  return Object.keys(WORKFLOW_FUNCTIONS).includes(event);
 }
