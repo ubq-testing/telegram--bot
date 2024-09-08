8b07280d634cf3c59aca62aeadd7eea60d846134
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-08 01:26:53 +0100

    chore: move and remove some

--- src/bot/workflow-functions/create-chat.ts
+++ src/bot/workflow-functions/create-chat.ts
@@ -3,29 +3,24 @@ import { CallbackResult } from "#root/types/proxy.js";
 import { MtProto } from "./bot/mtproto";
 
 export async function createChat(context: Context<"issues.labeled", SupportedEvents["issues.labeled"]>): Promise<CallbackResult> {
-    console.log("Creating chat");
     try {
-        const { payload } = context;
+        const { payload, env } = context;
         const chatName = payload.issue.title;
 
         const mtProto = new MtProto(context);
         await mtProto.initialize();
 
-        console.log("Creating chat with name: ", chatName);
-        const chat = await mtProto.client.invoke(
-            new mtProto.api.channels.CreateChannel({
+        context.logger.info("Creating chat with name: ", { chatName });
+        await mtProto.client.invoke(
+            new mtProto.api.messages.CreateChat({
                 title: chatName,
-                broadcast: true,
-                about: payload.issue.body || "No description provided",
+                users: [...env.BOT_ADMINS,],
             })
         );
-
-        console.log("Chat created: ", chat);
 
         return { status: 200, reason: "chat_created" };
     } catch (er) {
         context.logger.error("Failed to create chat", { er });
         return { status: 500, reason: "chat_creation_failed", content: { error: er } };
     }
-
 }
