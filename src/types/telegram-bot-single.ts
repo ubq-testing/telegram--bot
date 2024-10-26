00008bff7970b9070f039c4f7f9c4fc6c8094dbb
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-10-26 18:17:57 +0100

    chore: pass octo, try catch init calls

--- src/types/telegram-bot-single.ts
+++ src/types/telegram-bot-single.ts
@@ -2,6 +2,7 @@ import { Env } from ".";
 import { Bot, createBot } from "../bot";
 import { createServer } from "../server";
 import { logger } from "../utils/logger";
+import { PluginContext } from "./plugin-context-single";
 
 /**
  * Singleton for the worker instance of the telegram bot
@@ -16,28 +17,41 @@ export class TelegramBotSingleton {
   static async initialize(env: Env): Promise<TelegramBotSingleton> {
     const {
       TELEGRAM_BOT_ENV: {
-        botSettings: { TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_WEBHOOK, ALLOWED_UPDATES },
+        botSettings: { TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_WEBHOOK, ALLOWED_UPDATES, TELEGRAM_BOT_WEBHOOK_SECRET },
       },
     } = env;
 
+    const octokit = await PluginContext.getInstance().getTelegramEventOctokit();
+
     if (!TelegramBotSingleton._instance) {
       TelegramBotSingleton._instance = new TelegramBotSingleton();
-      TelegramBotSingleton._bot = createBot(TELEGRAM_BOT_TOKEN, {
-        config: env,
-        logger,
-      });
-      await TelegramBotSingleton._bot.api.setWebhook(TELEGRAM_BOT_WEBHOOK, {
-        allowed_updates: ALLOWED_UPDATES,
-        secret_token: env.TELEGRAM_BOT_ENV.botSettings.TELEGRAM_BOT_WEBHOOK_SECRET,
-      });
       try {
-        TelegramBotSingleton._server = createServer({
-          bot: TelegramBotSingleton._bot,
+        TelegramBotSingleton._bot = await createBot(TELEGRAM_BOT_TOKEN, {
           config: env,
           logger,
+          octokit,
         });
       } catch (er) {
         logger.error("Error initializing TelegramBotSingleton", { er });
+      }
+
+      try {
+        await TelegramBotSingleton._bot.api.setWebhook(TELEGRAM_BOT_WEBHOOK, {
+          allowed_updates: ALLOWED_UPDATES,
+          secret_token: TELEGRAM_BOT_WEBHOOK_SECRET,
+        });
+      } catch (er) {
+        logger.error("Error setting webhook in TelegramBotSingleton", { er });
+      }
+
+      try {
+        TelegramBotSingleton._server = createServer({
+          bot: TelegramBotSingleton._bot,
+          env,
+          logger,
+        });
+      } catch (er) {
+        logger.error("Error initializing server in TelegramBotSingleton", { er });
       }
     }
     return TelegramBotSingleton._instance;
