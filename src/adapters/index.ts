c8e12ebcc5527bfdcf98f98c8268c818180b15d0
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-10-27 17:23:19 +0000

    chore: tidy up supabase adapter

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,5 +1,21 @@
 import { Context } from "../types";
 import { SessionManagerFactory } from "../bot/mtproto-api/bot/session/session-manager";
+import { UserBaseStorage, ChatAction, HandleChatParams, StorageTypes, RetrievalHelper, Chat } from "../types/storage";
+
+export interface Storage {
+  userSnapshot(chatId: number, userIds: number[]): Promise<void>;
+  updateChatStatus(status: "open" | "closed" | "reopened", taskNodeId?: string, chatId?: number): Promise<void>;
+  saveChat(chatId: number, chatName: string, taskNodeId: string): Promise<void>;
+  retrieveChatByChatId(chatId: number): Promise<Chat | undefined>;
+  retrieveChatByTaskNodeId(taskNodeId: string): Promise<Chat | undefined>;
+  retrieveUserByTelegramId(telegramId: number, dbObj?: UserBaseStorage): Promise<UserBaseStorage | undefined>;
+  retrieveUserByGithubId(githubId: number | null | undefined, dbObj?: UserBaseStorage): Promise<UserBaseStorage | undefined>;
+  retrieveSession(): Promise<string | null>;
+  handleChat<TAction extends ChatAction>(params: HandleChatParams<TAction>): Promise<void>;
+  handleSession<TAction extends "create" | "delete">(session: string, action: TAction): Promise<void>;
+  handleUserBaseStorage<TType extends "create" | "delete" | "update">(user: UserBaseStorage, action: TType): Promise<boolean>;
+  storeData<TType extends StorageTypes>(data: RetrievalHelper<TType> | null, idToDelete?: number): Promise<boolean>;
+}
 
 export function createAdapters(ctx: Context) {
   const {
--- src/adapters/supabase/supabase.ts
+++ src/adapters/supabase/supabase.ts
@@ -1,39 +1,22 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 import { logger } from "../../utils/logger";
 import { Context } from "../../types";
-import { Chat, ChatAction, ChatStorage, HandleChatParams, RetrievalHelper, SessionStorage, StorageTypes, UserBaseStorage } from "../../types/storage";
+import { ChatAction, HandleChatParams, RetrievalHelper, StorageTypes, UserBaseStorage } from "../../types/storage";
+import { StorageGetters } from "./storage-getters";
+import { isChatsStorage, isUserBaseStorage, isSingleChatStorage, isSessionStorage } from "../storage-guards";
+import { Storage } from "..";
 
-export interface Storage {
-  userSnapshot(chatId: number, userIds: number[]): Promise<void>;
-  getChatUsers(chatId: number): Promise<number[]>;
-  getAllUsers(): Promise<UserBaseStorage[]>;
-  updateChatStatus(status: "open" | "closed" | "reopened", taskNodeId?: string, chatId?: number): Promise<void>;
-  saveChat(chatId: number, chatName: string, taskNodeId: string): Promise<void>;
-  retrieveChatByChatId(chatId: number): Promise<Chat | undefined>;
-  retrieveChatByTaskNodeId(taskNodeId: string): Promise<Chat | undefined>;
-  retrieveUserByTelegramId(telegramId: number, dbObj?: UserBaseStorage): Promise<UserBaseStorage | undefined>;
-  retrieveUserByGithubId(githubId: number | null | undefined, dbObj?: UserBaseStorage): Promise<UserBaseStorage | undefined>;
-  retrieveSession(): Promise<string | null>;
-  handleChat<TAction extends ChatAction>(params: HandleChatParams<TAction>): Promise<void>;
-  handleSession<TAction extends "create" | "delete">(session: string, action: TAction): Promise<void>;
-  handleUserBaseStorage<TType extends "create" | "delete" | "update">(user: UserBaseStorage, action: TType): Promise<boolean>;
-  storeData<TType extends StorageTypes>(data: RetrievalHelper<TType> | null, idToDelete?: number): Promise<boolean>;
-}
-
-export class SuperbaseStorage implements Storage {
+export class SuperbaseStorage extends StorageGetters implements Storage {
   protected supabase: SupabaseClient;
   protected logger = logger;
   octokit: Context["octokit"];
 
   constructor(ctx: Context, supabase: SupabaseClient) {
+    super(supabase);
     this.supabase = supabase;
     this.octokit = ctx.octokit;
   }
 
-  /**
-   * Used for storing the userIds of a chat before we
-   * ban all of them and archive the chat.
-   */
   async userSnapshot(chatId: number, userIds: number[]) {
     const chat = await this.retrieveChatByChatId(chatId);
     const { error } = await this.supabase.from("chats").update({ user_ids: userIds }).eq("chat_id", chat.chat_id);
@@ -42,31 +25,6 @@ export class SuperbaseStorage implements Storage {
     } else {
       this.logger.info("Successfully saved chat users", { chatId, userIds });
     }
-  }
-
-  /**
-   * Returns the userIds of a previously closed chat otherwise an empty array.
-   */
-  async getChatUsers(chatId: number) {
-    const { data, error } = await this.supabase.from("chats").select("user_ids").eq("chat_id", chatId).single();
-    if (error || !data) {
-      this.logger.error("No chat users found", { chatId });
-    } else {
-      this.logger.info("Successfully fetched chat users", { chatId });
-    }
-
-    return data?.user_ids || [];
-  }
-
-  async getAllUsers() {
-    const { data, error } = await this.supabase.from("userbase").select("*");
-    if (error || !data) {
-      this.logger.error("No users found", { er: error });
-    } else {
-      this.logger.info("Successfully fetched users");
-    }
-
-    return data || [];
   }
 
   async updateChatStatus(status: "open" | "closed" | "reopened", taskNodeId?: string, chatId?: number) {
@@ -106,64 +64,6 @@ export class SuperbaseStorage implements Storage {
     }
   }
 
-  async retrieveChatByChatId(chatId: number) {
-    const { data, error } = await this.supabase.from("chats").select("*").eq("chat_id", chatId).single();
-    if (error || !data) {
-      this.logger.error("No chat found", { chatId });
-    } else {
-      this.logger.info("Successfully fetched chat", { chatId });
-    }
-
-    return data;
-  }
-
-  async retrieveChatByTaskNodeId(taskNodeId: string) {
-    const { data, error } = await this.supabase.from("chats").select("*").eq("task_node_id", taskNodeId).single();
-    if (error || !data) {
-      this.logger.error("No chat found", { taskNodeId });
-    } else {
-      this.logger.info("Successfully fetched chat", { taskNodeId });
-    }
-
-    return data;
-  }
-
-  async retrieveUserByTelegramId(telegramId: number): Promise<UserBaseStorage | undefined> {
-    const { data, error } = await this.supabase.from("userbase").select("*").eq("telegram_id", telegramId).single();
-    if (error || !data) {
-      this.logger.error("No user found", { telegramId, err: error });
-    } else {
-      this.logger.info("Successfully fetched user", { telegramId });
-    }
-
-    return data;
-  }
-
-  async retrieveUserByGithubId(githubId: number | null | undefined): Promise<UserBaseStorage | undefined> {
-    if (!githubId) {
-      this.logger.error("No githubId provided to retrieve user");
-      return;
-    }
-
-    const { data, error } = await this.supabase.from("userbase").select("*").eq("github_id", githubId).single();
-    if (error || !data) {
-      this.logger.error("No user found", { githubId, err: error });
-    } else {
-      this.logger.info("Successfully fetched user", { githubId });
-    }
-
-    return data;
-  }
-
-  async retrieveSession() {
-    const { data, error } = await this.supabase.from("tg-bot-sessions").select("*").single();
-    if (error || !data) {
-      this.logger.error("No session found", { err: error });
-    } else {
-      return data.session;
-    }
-  }
-
   async handleChat<TAction extends ChatAction>(params: HandleChatParams<TAction>) {
     const { action, chat } = params;
     const nodeId = chat.task_node_id;
@@ -182,29 +82,18 @@ export class SuperbaseStorage implements Storage {
     }
   }
 
-  /**
-   * Consider "create" to be update as we'll delete the old session
-   * and replace it with the new one.
-   *
-   * "delete" will remove the session, this will break things without a new session.
-   */
   async handleSession<TAction extends "create" | "delete">(session: string, action: TAction) {
     if (action === "create") {
+      const { error } = await this.supabase.from("tg-bot-sessions").delete().single();
+      if (error) {
+        throw new Error("Failed to delete existing session");
+      }
       await this.storeData({ session });
     } else {
       await this.storeData({ session: null });
     }
   }
 
-  /**
-   * Updates the user bank with the new user data. If the user already exists,
-   * it will overwrite the existing data or delete it if the action is "delete".
-   *
-   * This is a Telegram focused method, so we'll use the telegramId as the key.
-   * This way notifications can only be received to the account which is subscribing,
-   * them may choose to listen into other users, but cannot enable notifications for
-   * another user.
-   */
   async handleUserBaseStorage<TType extends "create" | "delete" | "update">(user: UserBaseStorage, action: TType) {
     const existingUser = await this.retrieveUserByTelegramId(user.telegram_id);
 
@@ -224,13 +113,6 @@ export class SuperbaseStorage implements Storage {
     return true;
   }
 
-  /**
-   * Stores data in the GitHub repo. It will overwrite the existing
-   * data, so ensure you're passing the full object, not just the
-   * updated properties, or mistakes will be made.
-   *
-   * Do we need a safety check to ensure we are not accidentally deleting data? Maybe, needs tested.
-   */
   async storeData<TType extends StorageTypes>(data: RetrievalHelper<TType> | null, idToDelete?: number) {
     let type: StorageTypes | undefined;
 
@@ -274,25 +156,4 @@ export class SuperbaseStorage implements Storage {
       return false;
     }
   }
-}
-
-function isSessionStorage(data: unknown): data is SessionStorage {
-  if (typeof data !== "object" || !data) return false;
-  return "session" in data;
-}
-
-function isChatsStorage(data: unknown): data is ChatStorage {
-  if (typeof data !== "object" || !data) return false;
-  return "chats" in data;
-}
-
-function isUserBaseStorage(data: unknown): data is UserBaseStorage {
-  if (typeof data !== "object" || !data) return false;
-  const keys = Object.keys(data);
-  return keys.includes("telegram_id") && keys.includes("github_id") && keys.includes("listening_to") && keys.includes("additional_user_listeners");
-}
-
-function isSingleChatStorage(data: unknown): data is Chat {
-  if (typeof data !== "object" || !data) return false;
-  return "chat_id" in data || "task_node_id" in data;
 }
