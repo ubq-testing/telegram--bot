666712370a49af4afaec754ee7ead1df702130d0
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-31 12:41:38 +0900

    refactor(knip): update entry points and ignore dependencies
    Revised entry points for better workflow and streamlined dependencies.

--- .github/knip.ts
+++ .github/knip.ts
@@ -1,12 +1,12 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["build/index.ts"],
+  entry: ["src/worker.ts", "src/workflow-entry.ts"],
   project: ["src/**/*.ts"],
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@mswjs/data", "smee-client"],
+  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "smee-client"],
   eslint: true,
 };
 
