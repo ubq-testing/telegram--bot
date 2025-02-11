7acc8dde57ff3289cc542136c5d92be47e6d17aa
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-02-11 12:27:15 +0000

    refactor: action via sdk

--- src/workflow-entry.ts
+++ src/workflow-entry.ts
@@ -1,83 +1,102 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-import { Value } from "@sinclair/typebox/value";
-import { envValidator, pluginSettingsSchema, PluginInputs, pluginSettingsValidator } from "./types";
-import { PluginContext } from "./types/plugin-context-single";
+import { Context, envValidator, pluginSettingsSchema } from "./types";
 import dotenv from "dotenv";
-import { logger } from "./utils/logger";
-import { proxyWorkflowCallbacks } from "./handlers/workflow-proxy";
+import { createActionsPlugin } from "@ubiquity-os/plugin-sdk";
+import { LogLevel } from "@ubiquity-os/ubiquity-os-logger";
+import { runPlugin } from "./plugin";
+import { createAdapters } from "./adapters";
 dotenv.config();
 
-/**
- * Main entry point for the workflow functions
- */
-export async function run() {
-  const payload = github.context.payload.inputs;
-
-  let env, settings;
-
-  const payloadEnv = {
-    TELEGRAM_BOT_ENV: process.env.TELEGRAM_BOT_ENV,
-    APP_ID: process.env.APP_ID,
-    APP_PRIVATE_KEY: process.env.APP_PRIVATE_KEY,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    VOYAGEAI_API_KEY: process.env.VOYAGEAI_API_KEY || "", // not used through workflows
-  };
-
-  try {
-    env = Value.Decode(envValidator.schema, payloadEnv);
-  } catch (err) {
-    logger.error("Error decoding env: ", { err });
+export default createActionsPlugin(
+  (context) => {
+    const ctx = context as Context;
+    ctx.adapters = createAdapters(ctx);
+    return runPlugin(ctx);
+  },
+  {
+    logLevel: (process.env.LOG_LEVEL as LogLevel) ?? "debug",
+    settingsSchema: pluginSettingsSchema,
+    envSchema: envValidator.schema,
+    ...(process.env.KERNEL_PUBLIC_KEY && { kernelPublicKey: process.env.KERNEL_PUBLIC_KEY }),
+    postCommentOnError: true,
   }
+).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
 
-  try {
-    settings = Value.Decode(pluginSettingsSchema, Value.Default(pluginSettingsSchema, JSON.parse(payload.settings)));
-  } catch (err) {
-    logger.error("Error decoding settings: ", { err });
-  }
+/* eslint-disable */
 
-  if (!(settings && env)) {
-    throw new Error("Invalid settings or env provided");
-  }
+// /**
+//  * Main entry point for the workflow functions
+//  */
+// async function run() {
+//   const payload = github.context.payload.inputs;
 
-  if (!pluginSettingsValidator.test(settings)) {
-    throw new Error("Invalid settings provided");
-  }
+//   let env, settings;
 
-  const inputs: PluginInputs = {
-    stateId: payload.stateId,
-    eventName: payload.eventName,
-    eventPayload: JSON.parse(payload.eventPayload),
-    settings,
-    authToken: payload.authToken,
-    ref: payload.ref,
-    command: payload.command,
-    signature: payload.signature,
-  };
+//   const payloadEnv = {
+//     TELEGRAM_BOT_ENV: process.env.TELEGRAM_BOT_ENV,
+//     APP_ID: process.env.APP_ID,
+//     APP_PRIVATE_KEY: process.env.APP_PRIVATE_KEY,
+//     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+//     VOYAGEAI_API_KEY: process.env.VOYAGEAI_API_KEY || "", // not used through workflows
+//   };
 
-  PluginContext.initialize(inputs, env);
+//   try {
+//     env = Value.Decode(envValidator.schema, payloadEnv);
+//   } catch (err) {
+//     logger.error("Error decoding env: ", { err });
+//   }
 
-  const context = await PluginContext.getInstance().getContext();
-  return proxyWorkflowCallbacks(context)[inputs.eventName];
-}
+//   try {
+//     settings = Value.Decode(pluginSettingsSchema, Value.Default(pluginSettingsSchema, JSON.parse(payload.settings)));
+//   } catch (err) {
+//     logger.error("Error decoding settings: ", { err });
+//   }
 
-run()
-  .then((result) => {
-    core.setOutput("result", result);
-  })
-  .catch((err) => {
-    logger.error("Error running workflow: ", { err });
-    core.setFailed(err);
+//   if (!(settings && env)) {
+//     throw new Error("Invalid settings or env provided");
+//   }
 
-    if ("errorMessage" in err && err.errorMessage === "AUTH_KEY_DUPLICATED") {
-      /**
-       * Basically the server has decided that we are kicked at this point and
-       * will manual handling by a CODEOWNER or project admin that has the
-       * access needed in order to actually reset the auth key.
-       *
-       * We could try using the Bot API to send a message to the admins of the bot here
-       */
-    }
+//   if (!pluginSettingsValidator.test(settings)) {
+//     throw new Error("Invalid settings provided");
+//   }
 
-    process.exit(0);
-  });
+//   const inputs: PluginInputs = {
+//     stateId: payload.stateId,
+//     eventName: payload.eventName,
+//     eventPayload: JSON.parse(payload.eventPayload),
+//     settings,
+//     authToken: payload.authToken,
+//     ref: payload.ref,
+//     command: payload.command,
+//     signature: payload.signature,
+//   };
+
+//   PluginContext.initialize(inputs, env);
+
+//   const context = await PluginContext.getInstance().getContext();
+//   return proxyWorkflowCallbacks(context)[inputs.eventName];
+// }
+
+// run()
+//   .then((result) => {
+//     core.setOutput("result", result);
+//   })
+//   .catch((err) => {
+//     logger.error("Error running workflow: ", { err });
+//     core.setFailed(err);
+
+//     if ("errorMessage" in err && err.errorMessage === "AUTH_KEY_DUPLICATED") {
+//       /**
+//        * Basically the server has decided that we are kicked at this point and
+//        * will manual handling by a CODEOWNER or project admin that has the
+//        * access needed in order to actually reset the auth key.
+//        *
+//        * We could try using the Bot API to send a message to the admins of the bot here
+//        */
+//     }
+
+//     process.exit(0);
+//   });
+/* eslint-enable */
