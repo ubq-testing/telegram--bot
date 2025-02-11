785cc4080f0f84b7a5e40dfe5a48abf2d60596b9
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-02-11 13:30:09 +0000

    chore: replace sdk action with previous

--- src/workflow-entry.ts
+++ src/workflow-entry.ts
@@ -1,102 +1,102 @@
-import { Context, envValidator, pluginSettingsSchema } from "./types";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Value } from "@sinclair/typebox/value";
+import { envValidator, pluginSettingsSchema, PluginInputs, pluginSettingsValidator } from "./types";
+import { PluginContext } from "./types/plugin-context-single";
+import { logger } from "./utils/logger";
+import { proxyWorkflowCallbacks } from "./handlers/workflow-proxy";
 import dotenv from "dotenv";
-import { createActionsPlugin } from "@ubiquity-os/plugin-sdk";
-import { LogLevel } from "@ubiquity-os/ubiquity-os-logger";
-import { runPlugin } from "./plugin";
-import { createAdapters } from "./adapters";
 dotenv.config();
 
-export default createActionsPlugin(
-  (context) => {
-    const ctx = context as Context;
-    ctx.adapters = createAdapters(ctx);
-    return runPlugin(ctx);
-  },
-  {
-    logLevel: (process.env.LOG_LEVEL as LogLevel) ?? "debug",
-    settingsSchema: pluginSettingsSchema,
-    envSchema: envValidator.schema,
-    ...(process.env.KERNEL_PUBLIC_KEY && { kernelPublicKey: process.env.KERNEL_PUBLIC_KEY }),
-    postCommentOnError: true,
+async function run() {
+  const payload = github.context.payload.inputs;
+
+  let env, settings;
+
+  const payloadEnv = {
+    TELEGRAM_BOT_ENV: process.env.TELEGRAM_BOT_ENV,
+    APP_ID: process.env.APP_ID,
+    APP_PRIVATE_KEY: process.env.APP_PRIVATE_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    VOYAGEAI_API_KEY: process.env.VOYAGEAI_API_KEY ?? "", // not used through workflows
+  };
+
+  try {
+    env = Value.Decode(envValidator.schema, payloadEnv);
+  } catch (err) {
+    logger.error("Error decoding env: ", { err });
   }
-).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
 
-/* eslint-disable */
+  try {
+    settings = Value.Decode(pluginSettingsSchema, Value.Default(pluginSettingsSchema, JSON.parse(payload.settings)));
+  } catch (err) {
+    logger.error("Error decoding settings: ", { err });
+  }
 
-// /**
-//  * Main entry point for the workflow functions
-//  */
-// async function run() {
-//   const payload = github.context.payload.inputs;
+  if (!(settings && env)) {
+    throw new Error("Invalid settings or env provided");
+  }
 
-//   let env, settings;
+  if (!pluginSettingsValidator.test(settings)) {
+    throw new Error("Invalid settings provided");
+  }
 
-//   const payloadEnv = {
-//     TELEGRAM_BOT_ENV: process.env.TELEGRAM_BOT_ENV,
-//     APP_ID: process.env.APP_ID,
-//     APP_PRIVATE_KEY: process.env.APP_PRIVATE_KEY,
-//     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-//     VOYAGEAI_API_KEY: process.env.VOYAGEAI_API_KEY || "", // not used through workflows
-//   };
+  const inputs: PluginInputs = {
+    stateId: payload.stateId,
+    eventName: payload.eventName,
+    eventPayload: JSON.parse(payload.eventPayload),
+    settings,
+    authToken: payload.authToken,
+    ref: payload.ref,
+    command: payload.command,
+    signature: payload.signature,
+  };
 
-//   try {
-//     env = Value.Decode(envValidator.schema, payloadEnv);
-//   } catch (err) {
-//     logger.error("Error decoding env: ", { err });
+  PluginContext.initialize(inputs, env);
+
+  const context = await PluginContext.getInstance().getContext();
+  return proxyWorkflowCallbacks(context)[inputs.eventName];
+}
+
+run()
+  .then((result) => {
+    core.setOutput("result", result);
+  })
+  .catch((err) => {
+    logger.error("Error running workflow: ", { err });
+    core.setFailed(err);
+
+    if ("errorMessage" in err && err.errorMessage === "AUTH_KEY_DUPLICATED") {
+      /**
+       * Basically the server has decided that we are kicked at this point and
+       * will manual handling by a CODEOWNER or project admin that has the
+       * access needed in order to actually reset the auth key.
+       *
+       * We could try using the Bot API to send a message to the admins of the bot here
+       */
+    }
+
+    process.exit(0);
+  });
+
+
+
+// export default createActionsPlugin(
+//   (context) => {
+//     const ctx = context as Context;
+//     ctx.adapters = createAdapters(ctx);
+//     return runPlugin(ctx);
+//   },
+//   {
+//     logLevel: (process.env.LOG_LEVEL as LogLevel) ?? "debug",
+//     settingsSchema: pluginSettingsSchema,
+//     envSchema: envValidator.schema,
+//     ...(process.env.KERNEL_PUBLIC_KEY && { kernelPublicKey: process.env.KERNEL_PUBLIC_KEY }),
+//     postCommentOnError: true,
 //   }
+// ).catch((error) => {
+//   console.error(error);
+//   process.exit(1);
+// });
 
-//   try {
-//     settings = Value.Decode(pluginSettingsSchema, Value.Default(pluginSettingsSchema, JSON.parse(payload.settings)));
-//   } catch (err) {
-//     logger.error("Error decoding settings: ", { err });
-//   }
 
-//   if (!(settings && env)) {
-//     throw new Error("Invalid settings or env provided");
-//   }
-
-//   if (!pluginSettingsValidator.test(settings)) {
-//     throw new Error("Invalid settings provided");
-//   }
-
-//   const inputs: PluginInputs = {
-//     stateId: payload.stateId,
-//     eventName: payload.eventName,
-//     eventPayload: JSON.parse(payload.eventPayload),
-//     settings,
-//     authToken: payload.authToken,
-//     ref: payload.ref,
-//     command: payload.command,
-//     signature: payload.signature,
-//   };
-
-//   PluginContext.initialize(inputs, env);
-
-//   const context = await PluginContext.getInstance().getContext();
-//   return proxyWorkflowCallbacks(context)[inputs.eventName];
-// }
-
-// run()
-//   .then((result) => {
-//     core.setOutput("result", result);
-//   })
-//   .catch((err) => {
-//     logger.error("Error running workflow: ", { err });
-//     core.setFailed(err);
-
-//     if ("errorMessage" in err && err.errorMessage === "AUTH_KEY_DUPLICATED") {
-//       /**
-//        * Basically the server has decided that we are kicked at this point and
-//        * will manual handling by a CODEOWNER or project admin that has the
-//        * access needed in order to actually reset the auth key.
-//        *
-//        * We could try using the Bot API to send a message to the admins of the bot here
-//        */
-//     }
-
-//     process.exit(0);
-//   });
-/* eslint-enable */
