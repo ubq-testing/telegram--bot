38cd3a842ff9cb5d47c27aa0ed1128aa7a94243b
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-02-11 14:42:31 +0000

    chore: knip

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,14 +1,7 @@
-import { Env, PluginInputs } from "./types";
 import { Context } from "./types";
-import { PluginContext } from "./types/plugin-context-single";
 import { proxyCallbacks } from "./handlers/worker-proxy";
 
 export async function runPlugin(context: Context) {
   const { eventName } = context;
   await Promise.resolve(proxyCallbacks(context)[eventName]);
-}
-
-export async function plugin(inputs: PluginInputs, env: Env) {
-  const context = await PluginContext.initialize(inputs, env).getContext();
-  return await runPlugin(context);
 }
