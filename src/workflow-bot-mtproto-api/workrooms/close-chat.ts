85579be1c0935bf5cdb6a5235d8fce2ce6b2813a
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-02-25 09:47:54 +0000

    refactor: replace MtProto with MtProtoWrapper in closeChat function and improve chat handling logic

--- src/workflow-bot-mtproto-api/workrooms/close-chat.ts
+++ src/workflow-bot-mtproto-api/workrooms/close-chat.ts
@@ -1,122 +1,96 @@
-import { MtProto } from "../bot/mtproto";
+import { MtProtoWrapper } from "../bot/mtproto-wrapper";
 import { Api } from "telegram";
 import bigInt from "big-integer";
 import { Context } from "../../types";
 import { CallbackResult } from "../../types/proxy";
+import { GithubStorage } from "../../adapters/github/storage-layer";
+import { SuperbaseStorage } from "../../adapters/supabase/supabase";
+import { Chat } from "../../types/storage";
 
 export async function closeChat(context: Context<"issues.closed">): Promise<CallbackResult> {
-  const {
-    payload,
-    adapters: { storage },
-    logger,
-  } = context;
+  const { payload, adapters: { storage }, logger, } = context;
   if (payload.repository.full_name.includes("devpool-directory")) {
     return { status: 200, reason: "skipped" };
   }
 
-  const mtProto = new MtProto(context);
-  await mtProto.initialize();
-
-  logger.info("Closing chat with name: ", { chatName: payload.issue.title });
+  const mtProtoWrapper = new MtProtoWrapper(context);
+  await mtProtoWrapper.initialize();
   const dbChat = await storage.retrieveChatByTaskNodeId(payload.issue.node_id);
-
   if (!dbChat) {
-    logger.error("Chat not found in database", { chatName: payload.issue.title });
+    logger.error("Attempted to close a workroom not found in the database.", { chatName: payload.issue.title });
     return { status: 200, reason: "chat_not_found" };
   }
 
   logger.info("Chat found: ", { dbChat });
 
-  await mtProto.client.getDialogs();
-  const fetchedChat = await mtProto.client.invoke(
-    new mtProto.api.messages.GetFullChat({
-      chatId: bigInt(dbChat.chat_id),
-    })
-  );
+  await mtProtoWrapper.getMtProtoClient().getDialogs();
+  const chatParticipants = await mtProtoWrapper.getChatParticipants(dbChat);
 
-  if (!fetchedChat) {
-    throw new Error("Failed to fetch chat");
-  }
+  await mtProtoWrapper.updateChatArchiveStatus({ archive: true, dbChat });
+  await notifyAndRemoveFromArchivedChat({ mtProtoWrapper, chatParticipants, dbChat, storage, context });
 
-  let chatParticipants;
+  await storage.handleChat({ action: "close", chat: dbChat });
+  return { status: 200, reason: "chat_closed" };
+}
 
-  if ("participants" in fetchedChat.fullChat) {
-    chatParticipants = fetchedChat.fullChat.participants;
-  } else {
-    throw new Error("Failed to fetch chat participants");
-  }
-
-  // archive it
-  await mtProto.client.invoke(
-    new mtProto.api.folders.EditPeerFolders({
-      folderPeers: [
-        new mtProto.api.InputFolderPeer({
-          peer: new mtProto.api.InputPeerChat({ chatId: bigInt(dbChat.chat_id) }),
-          folderId: 1, // 0 is active, 1 is archived
-        }),
-      ],
-    })
-  );
-
+async function notifyAndRemoveFromArchivedChat({ mtProtoWrapper, chatParticipants, dbChat, storage, context }:
+  {
+    mtProtoWrapper: MtProtoWrapper;
+    chatParticipants: Api.TypeChatParticipants,
+    dbChat: Chat,
+    storage: GithubStorage | SuperbaseStorage,
+    context: Context<"issues.closed">,
+  }) {
   if (chatParticipants.className === "ChatParticipants") {
-    await mtProto.client.invoke(
-      new mtProto.api.messages.SendMessage({
-        message: "This task has been closed and this chat has been archived.",
-        peer: new mtProto.api.InputPeerChat({ chatId: bigInt(dbChat.chat_id) }),
-      })
-    );
-
+    await mtProtoWrapper.sendMessageToChat(dbChat, "This task has been closed and this chat has been archived.");
     const participants = chatParticipants.participants;
-    let creatorId;
+    const creatorId = (await mtProtoWrapper.getChatCreatorFromParticipants(participants)).userId;
+    const userIds = participants.map((participant) => participant.userId).filter((id) => id !== undefined);
 
-    const userIds = participants
-      .map((participant) => {
-        if (participant.className === "ChatParticipantCreator") {
-          creatorId = participant.userId;
-          return undefined;
-        }
-        return participant.userId;
-      })
-      .filter((id) => id !== undefined);
-
-    if (!creatorId) {
-      throw new Error("Failed to get chat creator");
+    if (!userIds.includes(creatorId)) {
+      userIds.push(creatorId);
     }
-
-    userIds.push(creatorId);
-    const chatInput = await mtProto.client.getInputEntity(dbChat.chat_id);
 
     await storage.userSnapshot(
       dbChat.chat_id,
       userIds.map((id) => id.toJSNumber())
     );
 
-    let generator = deleteChatUsers(mtProto, userIds, context, chatInput);
+    const mtProtoClient = mtProtoWrapper.getMtProtoClient();
+    const mtProtoApi = mtProtoWrapper.getMtProtoApi();
+
+    let generator = deleteChatUsers({
+      mtProtoClient,
+      api: mtProtoApi,
+      userIds, context,
+      chatInputEntity: await mtProtoClient.getInputEntity(dbChat.chat_id)
+    });
+
     let result = await generator.next();
+    const chatInputEntity = await mtProtoClient.getInputEntity(dbChat.chat_id);
 
     while (!result.done) {
       if (!result.value.success) {
         const seconds = result.value.error?.seconds ?? 300;
-        logger.info("Retrying...", { seconds, er: result.value.error });
-
-        await intervalLogger(seconds, 60, logger, new Promise((resolve) => setTimeout(resolve, seconds * 1000)));
-
-        generator = deleteChatUsers(mtProto, userIds.slice(result.value.index), context, chatInput);
+        context.logger.info("Retrying...", { seconds, er: result.value.error });
+        await intervalLogger(seconds, 60, context.logger, new Promise((resolve) => setTimeout(resolve, seconds * 1000)));
+        generator = deleteChatUsers({ mtProtoClient, api: mtProtoApi, userIds: userIds.slice(result.value.index), context, chatInputEntity });
       }
       result = await generator.next();
     }
   }
-
-  await storage.handleChat({ action: "close", chat: dbChat });
-  return { status: 200, reason: "chat_closed" };
 }
 
 async function* deleteChatUsers(
-  mtProto: MtProto,
-  userIds: bigInt.BigInteger[],
-  context: Context,
-  chatInput: Api.TypeInputPeer
-): AsyncGenerator<{ success: boolean; index: number; error?: { errorMessage: string; seconds: number } }> {
+  { mtProtoClient, api, userIds, context, chatInputEntity }:
+    {
+      mtProtoClient: MtProtoWrapper["_client"];
+      api: MtProtoWrapper["_api"];
+      userIds: bigInt.BigInteger[];
+      context: Context; chatInputEntity:
+      Api.TypeInputPeer;
+    })
+  : AsyncGenerator<{ success: boolean; index: number; error?: { errorMessage: string; seconds: number } }> {
   for (let i = 0; i < userIds.length; i++) {
     // don't kick our friendly bot
     if (userIds[i].toJSNumber() === context.config.botId) {
@@ -124,10 +98,10 @@ async function* deleteChatUsers(
     }
 
     try {
-      await mtProto.client.invoke(
-        new mtProto.api.messages.DeleteChatUser({
+      await mtProtoClient.invoke(
+        new api.messages.DeleteChatUser({
           revokeHistory: false,
-          chatId: chatInput.className === "InputPeerChat" ? chatInput.chatId : undefined,
+          chatId: chatInputEntity.className === "InputPeerChat" ? chatInputEntity.chatId : undefined,
           userId: userIds[i],
         })
       );
