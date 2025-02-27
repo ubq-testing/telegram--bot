42cd25ed8a3a0d1c6fadd23c55f7690faa9e3ead
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-02-27 12:26:29 +0000

    feat: add ReviewNotificationHandler for handling pull request review requests

---
+++ src/github-handlers/private-notifications/handler-review-requests.ts
@@ -0,0 +1,34 @@
+import { NotificationHandlerBase } from "./notification-handler-base";
+import { Context } from "../../types";
+import { CallbackResult } from "../../types/proxy";
+import { NotificationMessage } from "./notification-message";
+import { StorageUser } from "../../types/storage";
+
+class ReviewNotificationHandler extends NotificationHandlerBase<"pull_request.review_requested"> {
+  protected getUserId() {
+    return this.context.payload.requested_reviewer?.id;
+  }
+
+  protected shouldSkipNotification(dbUser: StorageUser) {
+    const requestedReviewer = this.context.payload.requested_reviewer?.login;
+    return this.context.payload.sender.login === requestedReviewer || !dbUser.listening_to["review"];
+  }
+
+  protected getMessage(username: string) {
+    const prAuthor = this.context.payload.pull_request.user?.login ?? this.context.payload.pull_request.user?.name;
+    const ownerRepo = this.context.payload.repository.full_name;
+    const issueNumber = this.context.payload.pull_request.number;
+    return NotificationMessage.getReviewMessage({
+      pullRequestHtmlUrl: this.context.payload.pull_request.html_url,
+      repositoryFullName: ownerRepo,
+      pullRequestAuthor: prAuthor ?? "Unknown",
+      username,
+      issueNumber: issueNumber.toString(),
+    });
+  }
+}
+
+export async function reviewNotification(context: Context<"pull_request.review_requested">): Promise<CallbackResult> {
+  const handler = new ReviewNotificationHandler(context);
+  return handler.handle();
+}
