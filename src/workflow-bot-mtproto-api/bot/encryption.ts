4961b56cee54a913885fcd3277171811f84672d0
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-03-25 04:57:56 +0000

    refactor: enhance decryption error handling and logging

--- src/workflow-bot-mtproto-api/bot/encryption.ts
+++ src/workflow-bot-mtproto-api/bot/encryption.ts
@@ -1,7 +1,17 @@
 import CryptoJS from "crypto-js";
 
 export function decrypt(privateKey: string, text: string): string {
-  return CryptoJS.AES.decrypt(text, privateKey).toString();
+  try {
+    return CryptoJS.AES.decrypt(text, privateKey).toString(CryptoJS.enc.Utf8);
+  } catch (e) {
+    console.log("Failed to decrypt text", {
+      error: e,
+      text,
+      pk: privateKey.substring(0, 10),
+    });
+
+    throw new Error("Failed to decrypt text");
+  }
 }
 
 export function encrypt(privateKey: string, text: string): string {
